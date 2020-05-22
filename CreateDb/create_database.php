a07e0eec60c09669589f153221fdcbd4618ce2ad
AmitKhan012 <59249037+AmitKhan012@users.noreply.github.com>
2020-05-23 01:28:01 +0600

    Update create_database.php

--- CreateDb/create_database.php
+++ CreateDb/create_database.php
@@ -6,23 +6,23 @@
 
 	$db_create->query('CREATE TABLE `legal_counselling`.`clients` (
 	  `username` varchar(50)  NOT NULL,
-  	`first_name` VARCHAR(50),
-   	`last_name` VARCHAR(50),
-   	`email` VARCHAR(50),
-    `password` VARCHAR(50) NOT NULL,
-    `contract_number` VARCHAR(12) ,
-    `gender` VARCHAR(8),
-	PRIMARY KEY (`username`) 
-);') or die('clients table creation failed');	
+  	  `first_name` VARCHAR(50),
+   	  `last_name` VARCHAR(50),
+   	  `email` VARCHAR(50),
+          `password` VARCHAR(50) NOT NULL,
+          `contract_number` VARCHAR(12) ,
+          `gender` VARCHAR(8),
+	   PRIMARY KEY (`username`) 
+        );') or die('clients table creation failed');	
 	$db_create->query('CREATE TABLE `legal_counselling`.`lawers` (
 	  `username` varchar(50)  NOT NULL,
-  	`first_name` VARCHAR(50),
-   	`last_name` VARCHAR(50),
-   	`email` VARCHAR(50),
-    `password` VARCHAR(50) NOT NULL,
-    `contract_number` VARCHAR(12) ,
-    `gender` VARCHAR(8),
-	PRIMARY KEY (`username`) 
-);') or die('lawers table creation failed');	
+  	  `first_name` VARCHAR(50),
+   	  `last_name` VARCHAR(50),
+   	  `email` VARCHAR(50),
+          `password` VARCHAR(50) NOT NULL,
+          `contract_number` VARCHAR(12) ,
+          `gender` VARCHAR(8),
+	  PRIMARY KEY (`username`) 
+	);') or die('lawers table creation failed');	
   
 ?>
